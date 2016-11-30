63d68b3becf2b64377134dfb2f88ddc83e21d3dc
Max Odnovolyk <modnovolyk@gmail.com>
2016-12-01 01:14:21 +0200

    Update tests to be Linux compatible

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,78 @@
+//
+//  LinuxMain.swift
+//  MAVLink
+//
+//  Created by Max Odnovolyk on 12/1/16.
+//  Copyright © 2016 Build Apps. All rights reserved.
+//
+
+#if os(Linux)
+    
+import XCTest
+@testable import LinuxMainGeneratorTests
+ 
+XCTMain([
+        testCase(CompareWithCImplementationResultsTest.allTests),
+        testCase(DataExtensionsTests.allTests),
+        testCase(MAVLinkTests.allTests)
+    ])
+    
+extension CompareWithCImplementationResultsTest {
+    static let allTests = [
+        ("testCImplementationDidParseMessageThatStartsRightAfterCorruptedCRCByte", testCImplementationDidParseMessageThatStartsRightAfterCorruptedCRCByte),
+        ("testCImplementationDidParseMessageThatStartsRightAfterCorruptedMessageIdByte", testCImplementationDidParseMessageThatStartsRightAfterCorruptedMessageIdByte),
+        ("testCImplementationDidParseSwiftMAVLinkOutput", testCImplementationDidParseSwiftMAVLinkOutput),
+        ("testSwiftParserImplementationDidReturnSameFloatValuesAsCImplementation", testSwiftParserImplementationDidReturnSameFloatValuesAsCImplementation),
+        ("testSwiftParserImplementationDidReturnSameNumberOfMessagesAsCImplementation", testSwiftParserImplementationDidReturnSameNumberOfMessagesAsCImplementation)
+    ]
+}
+    
+extension DataExtensionsTests {
+    static let allTests = [
+        ("testGetArrayCannotAccessBytesOutOfDataUpperBound", testGetArrayCannotAccessBytesOutOfDataUpperBound),
+        ("testGetArrayDidGetArrayAtOffset", testGetArrayDidGetArrayAtOffset),
+        ("testGetArrayDidGetRequestedNumberOfItems", testGetArrayDidGetRequestedNumberOfItems),
+        ("testGetArrayDidGetSameValuePreviouslySetWithSetArrayCall", testGetArrayDidGetSameValuePreviouslySetWithSetArrayCall),
+        ("testGetEnumerationCannotInitWithUnknownValue", testGetEnumerationCannotInitWithUnknownValue),
+        ("testGetEnumerationDidGetProperCaseAtOffset", testGetEnumerationDidGetProperCaseAtOffset),
+        ("testGetEnumerationDidGetSameValuePreviouslySetWithSetEnumerationCall", testGetEnumerationDidGetSameValuePreviouslySetWithSetEnumerationCall),
+        ("testGetNumberByteOrderIsSwappedOnLittleEndianSystem", testGetNumberByteOrderIsSwappedOnLittleEndianSystem),
+        ("testGetNumberByteOrderRemainsSameOnBigEndianSystem", testGetNumberByteOrderRemainsSameOnBigEndianSystem),
+        ("testGetNumberCannotGetTooLargeNumber", testGetNumberCannotGetTooLargeNumber),
+        ("testGetNumberDidGetNumberAtOffset", testGetNumberDidGetNumberAtOffset),
+        ("testGetNumberDidGetSameValuePreviouslySetWithSetNumberCall", testGetNumberDidGetSameValuePreviouslySetWithSetNumberCall),
+        ("testGetStringCannotAccessBytesOutOfDataUpperBound", testGetStringCannotAccessBytesOutOfDataUpperBound),
+        ("testGetStringCannotReadNonASCIISEncodedString", testGetStringCannotReadNonASCIISEncodedString),
+        ("testGetStringDidGetSameValuePreviouslySetWithSetStringCall", testGetStringDidGetSameValuePreviouslySetWithSetStringCall),
+        ("testGetStringDidReadASCIIEncodedStringAtSpecifiedOffset", testGetStringDidReadASCIIEncodedStringAtSpecifiedOffset),
+        ("testGetStringDidReadEmptyNullTerminatedASCIIEncodedString", testGetStringDidReadEmptyNullTerminatedASCIIEncodedString),
+        ("testGetStringDidReadExactlyLengthSizedASCIIEncodedStringWithoutNullTermination", testGetStringDidReadExactlyLengthSizedASCIIEncodedStringWithoutNullTermination),
+        ("testGetStringDidReadNullTerminatedASCIIEncodedString", testGetStringDidReadNullTerminatedASCIIEncodedString),
+        ("testSetArrayCannotSetArrayWithLessCapacityThanArrayCount", testSetArrayCannotSetArrayWithLessCapacityThanArrayCount),
+        ("testSetArrayCannotSetValuesOutOfDataUpperBound", testSetArrayCannotSetValuesOutOfDataUpperBound),
+        ("testSetArrayDidSetValuesAtOffset", testSetArrayDidSetValuesAtOffset),
+        ("testSetEnumerationDidSetProperRawValueAtOffset", testSetEnumerationDidSetProperRawValueAtOffset),
+        ("testSetNumberByteOrderIsSwappedOnLittleEndianSystem", testSetNumberByteOrderIsSwappedOnLittleEndianSystem),
+        ("testSetNumberByteOrderRemainsSameOnBigEndianSystem", testSetNumberByteOrderRemainsSameOnBigEndianSystem),
+        ("testSetNumberCannotSetTooLargeNumber", testSetNumberCannotSetTooLargeNumber),
+        ("testSetNumberDidSetNumberAtOffset", testSetNumberDidSetNumberAtOffset),
+        ("testSetStringCannotSetNonASCIIString", testSetStringCannotSetNonASCIIString),
+        ("testSetStringCanntSetTooLongString", testSetStringCanntSetTooLongString),
+        ("testSetStringDidSetEmptyString", testSetStringDidSetEmptyString),
+        ("testSetStringDidSetNullTerminatedStringAtOffset", testSetStringDidSetNullTerminatedStringAtOffset)
+    ]
+}
+    
+extension MAVLinkTests {
+    static let allTests = [
+        ("testDispatchDidPutProperComponentId", testDispatchDidPutProperComponentId),
+        ("testDispatchDidPutProperCRC", testDispatchDidPutProperCRC),
+        ("testDispatchDidPutProperMessageId", testDispatchDidPutProperMessageId),
+        ("testDispatchDidPutProperSystemId", testDispatchDidPutProperSystemId),
+        ("testDispatchRethrowsDataExtensionsErrors", testDispatchRethrowsDataExtensionsErrors),
+        ("testParseDidParseMessageThatStartsRightAfterCorruptedCRCByte", testParseDidParseMessageThatStartsRightAfterCorruptedCRCByte),
+        ("testParseDidParseMessageThatStartsRightAfterCorruptedMessageIdByte", testParseDidParseMessageThatStartsRightAfterCorruptedMessageIdByte)
+    ]
+}
+    
+#endif
